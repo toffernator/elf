cb5bcf28b403e6f0b6569876918884df694c0225
Christoffer Gram <74768013+toffernator@users.noreply.github.com>
2024-02-27 21:28:44 +0100

    feat(tooling): Build as Default Package in flake.nix
    So you can do `nix run`

--- flake.nix
+++ flake.nix
@@ -8,12 +8,19 @@
       "github:NixOS/nixpkgs/d934204a0f8d9198e1e4515dd6fec76a139c87f0";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
+        packages.default = pkgs.buildGo122Module {
+          name = "elf";
+          version = "0.0.1";
+          src = self;
+          vendorHash = null;
+        };
+
         devShells.default =
-          pkgs.mkShell { packages = with pkgs; [ just go_1_22 bruno ]; };
+          pkgs.mkShell { packages = with pkgs; [ go_1_22 bruno ]; };
       });
 }
 
